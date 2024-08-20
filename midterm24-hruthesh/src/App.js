32c9f860eb272afc2a7c85513a8d60f29e53249a
Hrut7hesh <gelle.hrutheshredd@gmail.com>
2024-08-20 18:02:26 +0530

    Midterm Assignment completed

--- midterm24-hruthesh/src/App.js
+++ midterm24-hruthesh/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect ,Suspense, lazy} from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { loginaction } from './actions/loginActions';
@@ -7,9 +7,9 @@ import RegistrationPage from './components/RegistrationPage';
 import UserDetails from './components/MembersPage';
 import Navbar from './components/Navbar'
 import ShowProducts from './components/ShowProducts';
-import AddCategory from './components/admin/AddCategory';
-import AddProducts from './components/admin/AddProducts';
-import Admin from './components/Admin';
+const Admin = lazy(() => import('./components/Admin'));
+const AddCategory = lazy(() => import('./components/admin/AddCategory'));
+const AddProducts = lazy(() => import('./components/admin/AddProducts'));
 
 const App = () => {
     const dispatch = useDispatch();
@@ -27,14 +27,34 @@ const App = () => {
             <Routes>
                 <Route path="/Home" element={<ShowProducts />} />
                 <Route path="/Home/:id" element={<ShowProducts />} />
-                <Route path="/admin" element={<Admin />}>
-                    <Route path="addcategory" element={<AddCategory />} />
-                    <Route path="addproducts" element={<AddProducts />} />
+                <Route
+                    path="/admin"
+                    element={
+                        <Suspense fallback={<div>Loading Admin...</div>}>
+                            <PrivateRoute element={<Admin />} />
+                        </Suspense>
+                    }
+                >
+                    <Route
+                        path="addcategory"
+                        element={
+                            <Suspense fallback={<div>Loading Add Category...</div>}>
+                                <AddCategory />
+                            </Suspense>
+                        }
+                    />
+                    <Route
+                        path="addproducts"
+                        element={
+                            <Suspense fallback={<div>Loading Add Products...</div>}>
+                                <AddProducts />
+                            </Suspense>
+                        }
+                    />
                 </Route>
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/register" element={<RegistrationPage />} />
                 <Route path="/members" element={<PrivateRoute element={<UserDetails />} />} />
-                <Route path="/admin" element={<PrivateRoute element={<Admin />} />} />
             </Routes>
         </Router>
     );
